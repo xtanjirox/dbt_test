8cfbf47da7b5ca124da98aeb05a60d0fca1b9241
pharowmeza <mehdi@pharow.com>
2025-12-18 11:59:42 +0100

    test

--- models/marts/customer_orders.sql
+++ models/marts/customer_orders.sql
@@ -1,23 +1,12 @@
 with customers as (
     select * from {{ ref('stg_customers') }}
 ),
-
-orders as (
-    select * from {{ ref('stg_orders') }}
-),
-
 customer_order_summary as (
     select
         c.customer_id,
         c.customer_name,
-        c.country,
-        count(o.order_id) as total_orders,
-        sum(o.amount) as total_amount,
-        min(o.order_date) as first_order_date,
-        max(o.order_date) as last_order_date
+        c.country
     from customers c
-    left join orders o on c.customer_id = o.customer_id
-    group by c.customer_id, c.customer_name, c.country
 )
 
 select * from customer_order_summary
